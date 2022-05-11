513c34019f55863826f73ee2f0c76fecd179e4f2
MiguelLamas <lamasmigs@yahoo.co.uk>
2022-05-11 23:00:51 +0100

    Clean Up Task 1

--- task-1/main.js
+++ task-1/main.js
@@ -1,3 +1,20 @@
+/*
+// TASK 1.1
+- CREATE A FUNCTION CALLED makeLegend WITH SOLE PARAMETER OF (NAME) WHICH TAKES IN
+('name') STRING AS SOLE ARGUMENT.
+
+// TASK 1.2
+- CREATE A VARIABLE CALLED legendaryCelebs EQUAL TO AN EMPTY ARRAY = [] INSIDE FUNCTION BLOCK.
+- CREATE A FOR LOOP TO ITERATE THROUGH VARIABLE WITH ARRAY OF CELEBRITY NAMES ('STRINGS').
+- CONCATENATE CELEBS ON ARRAY AND NEW STRING AND PUSH INTO NEWLY CREATED legendaryCelebs ARRAY VARIABLE.
+
+//TASK 1.3
+- CREATE A NEW EMPTY ARRAY AND STORE IN VARIABLE CALLED vowelCelebs
+- MAKE A FOR LOOP TO ITERATE THROUGH CELEB ARRAY AND IF CELEB NAME STARTS WITH A VOWEL
+PUSH ONTO NEW vowelCelebs array variable.
+- Print vowelCelebs.
+*/
+
 let celebs = [
   "David Beckham",
   "Cher",
@@ -11,44 +28,17 @@ let celebs = [
   "Emma Thompson",
 ];
 
-/*
-// TASK 1.1
-- CREATE A FUNCTION CALLED makeLegend WITH SOLE PARAMETER OF (NAME) WHICH TAKES IN
-('name') STRING AS SOLE ARGUMENT.
-
-/*
-function makeLegend(str) {
-  return `${str} is now a legend`;
-}
-
-console.log(makeLegend('Chris')); */
-
-/*
-// TASK 1.2
-- CREATE A VARIABLE CALLED legendaryCelebs EQUAL TO AN EMPTY ARRAY = [] INSIDE FUNCTION BLOCK.
-- CREATE A FOR LOOP TO ITERATE THROUGH VARIABLE WITH ARRAY OF CELEBRITY NAMES ('STRINGS').
-- CONCATENATE CELEBS ON ARRAY AND NEW STRING AND PUSH INTO NEWLY CREATED legendaryCelebs ARRAY VARIABLE.
-
-*/
-
 function makeLegend(str) {
   return `${str} is now a legend`;
 }
 
 let legendaryCelebs = [];
+
 for (let i = 0; i < celebs.length; i++) {
   legendaryCelebs.push(makeLegend(celebs[i]));
 }
 
 console.log(legendaryCelebs);
-
-/*
-//TASK 1.3
-- CREATE A NEW EMPTY ARRAY AND STORE IN VARIABLE CALLED vowelCelebs
-- MAKE A FOR LOOP TO ITERATE THROUGH CELEB ARRAY AND IF CELEB NAME STARTS WITH A VOWEL
-PUSH ONTO NEW vowelCelebs array variable.
-- Print vowelCelebs.
-*/
 
 let vowelCelebs = [];
 
